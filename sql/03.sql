e3dbad8ceec566596efb65992cd48ac43a2c2a11
tommy88520 <tommy8852024@gmail.com>
2021-07-31 23:38:08 +0800

    php+mql

--- sql/03.sql
+++ sql/03.sql
@@ -44,3 +44,29 @@ FROM `orders` o
         ON p.sid=d.product_sid
 WHERE o.sid=10
 
+-- 日期的格式 'YYY-MM-DD'
+SELECT * FROM `orders` WHERE `order_date` > '2017-01-01';
+
+-- '2017' > '2016-06-01 11:15:53'
+-- '2017' > '2018-06-01 11:15:53'
+
+-- 累加
+UPDATE `products` SET `price`=`price`+1 WHERE sid=1
+
+-- Datetime 不要用 + 運算
+SELECT NOW(), NOW()+100000;
+
+-- 子查詢
+SELECT product_sid sid FROM `order_details` WHERE `order_sid`=10;
+
+SELECT * FROM `products` WHERE sid IN (
+    SELECT product_sid FROM `order_details` WHERE `order_sid`=10
+    );
+
+SELECT * FROM `products`
+    JOIN (SELECT product_sid, price od_price FROM `order_details` WHERE `order_sid`=10) od
+        ON  `products`.sid = od.product_sid;
+
+-- 款式，型號，顏色
+
+-- 購物車：多種型態的商品、多個購物車
