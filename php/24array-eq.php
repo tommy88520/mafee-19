094a379cfa555d3f0d4c42b34c154755ac0508a1
tommy88520 <tommy8852024@gmail.com>
2021-08-03 11:55:42 +0800

    test2

--- php/24array-eq.php
+++ php/24array-eq.php
@@ -1,18 +1,24 @@
-<pre>
-    
-    <?php
-    $ar = array(
-        123,
-        'name' => '李曉明/帥',
-        'age' => 36,
-        'gender' => 'male',
-        'hellow',
-    
-    );
+<h3>
+<?php
+$ar = array(
+    'name' => '李小明',
+    'age' => 23,
+    'gender' => '男生',
+);
+$ar2 = $ar;  // 複製新的陣列再指定 (設定值) , set by value
+$ar2['name'] = '陳小華';
 
-    $ar2 = $ar;
-    $ar2['name'] = '黃彥銘';
-    echo
-    ?>
-    
-</pre>
+echo json_encode($ar, JSON_UNESCAPED_UNICODE);
+echo '<br>------------<br>';
+echo json_encode($ar2, JSON_UNESCAPED_UNICODE);
+echo '<br>------------<br>';
+
+$ar3 = &$ar;  // 設定位址, set by address
+$ar3['name'] = '許大功';
+
+echo json_encode($ar, JSON_UNESCAPED_UNICODE);
+echo '<br>------------<br>';
+echo json_encode($ar3, JSON_UNESCAPED_UNICODE);
+echo '<br>------------<br>';
+?>
+</h3>
