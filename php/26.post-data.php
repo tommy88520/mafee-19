e2985e2ef77aa2071e25549e3ba5c9a4bbf9c9b9
tommy88520 <tommy8852024@gmail.com>
2021-08-04 10:38:30 +0800

    ajax-fetch

--- php/26.post-data.php
+++ php/26.post-data.php
@@ -1,7 +1,7 @@
 
 <?php
 
-// echo $_GET['a'] ?? '';如果沒設定值 就是預設空字串
+// echo $_POST['a'] ?? '';如果沒設定值 就是預設空字串
 
 //設定回應給用用戶端的檔頭  對應requst.rest
 header('Content-Type: application/json');
@@ -13,7 +13,7 @@ $a = isset($_POST['a']) ? intval($_POST['a']) : 0 ;
 $b = isset($_POST['b']) ? intval($_POST['b']) : 0 ;
 
 echo json_encode([
-    'postData' => $_POST,
+    'POSTData' => $_POST,
     'result' => $a +$b,
 ]);
 // echo $a + $b;
