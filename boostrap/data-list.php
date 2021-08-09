b8af06725a14de9e195e09c10b23bc4a03d1359b
tommy88520 <tommy8852024@gmail.com>
2021-08-09 09:24:56 +0800

    datalist page

--- boostrap/data-list.php
+++ boostrap/data-list.php
@@ -14,16 +14,19 @@ $totalRows = $pdo->query("SELECT count(1) FROM address_book")->fetch(PDO::FETCH_
 
 // echo "$totalRows"; exit;
 //總共有幾頁,才能決定分頁按鈕
-$totalPages = ceil($totalRows/$perPage); // 正數是無條件進位
+$totalPages = ceil($totalRows / $perPage); // 正數是無條件進位
 // echo "$totalRows, $totalPages"; exit;
 
 //SELECT * FROM address_book ORDER BY sid DESC LIMIT 0,5
 //SELECT * FROM address_book ORDER BY sid DESC LIMIT 5,5 依此類推
-$sql = sprintf("SELECT * FROM address_book ORDER BY sid DESC LIMIT %s, %s",
-            ($page-1)*$perPage, $perPage);
+$sql = sprintf(
+    "SELECT * FROM address_book ORDER BY sid DESC LIMIT %s, %s",
+    ($page - 1) * $perPage,
+    $perPage
+);
 
 $rows = $pdo->query($sql)
-            ->fetchAll();
+    ->fetchAll();
 
 
 
@@ -33,31 +36,48 @@ $rows = $pdo->query($sql)
 <?php include __DIR__ . '/partials/html-head.php'; ?>
 <?php include __DIR__ . '/partials/nav-bar.php'; ?>
 <div class="container">
+    <div class="row justify-content-end">
+        <div class="col">
+            <nav aria-label="Page navigation example">
+                <ul class="pagination">
+    <!--                    <li class="page-item"><a class="page-link" href="#">Previous</a></li>-->
+
+                        <?php for($i=1; $i<=$totalPages; $i++): ?>
+                        <li class="page-item">
+                            <a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a>
+                        </li>
+                        <?php endfor; ?>
+
+    <!--                    <li class="page-item"><a class="page-link" href="#">Next</a></li>-->
+                    </ul>
+            </nav>
+        </div>
+    </div>
 
     <div class="row">
         <div class="col">
             <table class="table table-striped table-bordered">
                 <thead>
-                <tr>
-                    <th scope="col">sid</th>
-                    <th scope="col">name</th>
-                    <th scope="col">email</th>
-                    <th scope="col">mobile</th>
-                    <th scope="col">birthday</th>
-                    <th scope="col">address</th>
-                </tr>
+                    <tr>
+                        <th scope="col">sid</th>
+                        <th scope="col">name</th>
+                        <th scope="col">email</th>
+                        <th scope="col">mobile</th>
+                        <th scope="col">birthday</th>
+                        <th scope="col">address</th>
+                    </tr>
                 </thead>
                 <tbody>
-                <?php foreach($rows as $r): ?>
-                <tr>
-                    <td><?= $r['sid'] ?></td>
-                    <td><?= $r['name'] ?></td>
-                    <td><?= $r['email'] ?></td>
-                    <td><?= $r['mobile'] ?></td>
-                    <td><?= $r['birthday'] ?></td>
-                    <td><?= $r['address'] ?></td>
-                </tr>
-                <?php endforeach; ?>
+                    <?php foreach ($rows as $r) : ?>
+                        <tr>
+                            <td><?= $r['sid'] ?></td>
+                            <td><?= $r['name'] ?></td>
+                            <td><?= $r['email'] ?></td>
+                            <td><?= $r['mobile'] ?></td>
+                            <td><?= $r['birthday'] ?></td>
+                            <td><?= $r['address'] ?></td>
+                        </tr>
+                    <?php endforeach; ?>
                 </tbody>
             </table>
 
--- boostrap/partials/init.php
+++ boostrap/partials/init.php
@@ -2,6 +2,9 @@
 
 require __DIR__. '/db_connect.php';
 
+// if(! isset($_SESSION)){
+//     session_start();
+// } 可以判斷require的檔案有沒重複啟用session
 
 // 如果沒有啟用 session, 就不啟用
 if(! isset($_SESSION)) {
